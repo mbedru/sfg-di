8b6cb5efc297180dbf9de346247ad560efc96b56
mame <mbedru18@yahoo.com>
2022-09-24 14:22:10 +0300

    Assignment-4 Dependency Injection

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -11,6 +11,11 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
+		//////////////////////////////////////
+		PetController petController = (PetController) ctx.getBean("petController"/*, PetController.class*/);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
+		///////////////////////////////////
 		I18nController i18nController = (I18nController)ctx.getBean("i18nController");
 		System.out.println("-----I18----");
 		System.out.println(i18nController.sayHello());
