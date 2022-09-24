952c3e95dc63e1295f43b6c3f72865eafe488ecb
mame <mbedru18@yahoo.com>
2022-09-24 13:18:36 +0300

    work on feature 2.0 freaking out

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -33,5 +33,8 @@ public class SfgDiApplication {
 		System.out.println(constructorInjectedController.getGreeting());
 
 	}
+	public void hello () {
+		System.out.println("helloassignment");
+	}
 
 }
